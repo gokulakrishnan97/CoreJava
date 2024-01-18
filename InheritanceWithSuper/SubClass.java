efd679f05c48add593da315efe7589d87a16afe4
Gokul <gokulakrishnanit1997@gmail.com>
2024-01-19 02:03:25 +0530

    Inheritance woth super keyword functionality

--- InheritanceWithSuper/SubClass.java
+++ InheritanceWithSuper/SubClass.java
@@ -1,5 +1,8 @@
 class SuperClass{
-	int num = 20;
+	int num;
+	SuperClass(int num){
+		this.num = num;
+	}
 	public void display(){
 		System.out.println("Display the super class");
 	}
@@ -7,9 +10,14 @@ class SuperClass{
 
 public class SubClass extends SuperClass{
 	int num = 30;
+
+	SubClass(){
+		super(20);
+	}
 	
 	public void display(){
 		System.out.println("Display the sub class");
+		System.out.println("Super class value " + super.num);
 	}
 
 	public void showItems(){
@@ -21,5 +29,7 @@ public class SubClass extends SuperClass{
 	public static void main(String args[]){
 		SubClass sb2 = new SubClass();
 		sb2.showItems();
+		System.out.println(sb2.num);
+
 	}
 }
